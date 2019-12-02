78d15cc9bacfc37215a525f90306cffe7efc2318
1500641 <1500641@edu.karelia.fi>
2019-12-02 13:16:03 +0200

    Base project, contains debug bin with input file
    Contains simple file reader for the tasks, contains debug folder with required input file for tests.

---
+++ CodeAdvent/FileReader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CodeAdvent
+{
+    public static class FileReader
+    {
+        public static List<string> ReadFile(string path)
+        {
+            List<string> list = new List<string>();
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    list.Add(line);
+                }
+            }
+
+            return list;
+        }
+    }
+}
---
+++ CodeAdvent/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CodeAdvent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = Environment.CurrentDirectory + "/Input/Input.txt";
+
+            //string filePath = Path.co Directory.GetParent(workingDirectory).Parent.FullName + "@~/Input/Input.txt";
+
+            List<string> fileContents = FileReader.ReadFile(filePath);
+        }
+
+    }
+}
